b9d7d2e303df53ce71b731b54b33f02aff395c10
emicklei_kramphub <ernest@kramphub.com>
2019-12-07 05:36:32 +0100

    use v3 import for log

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/emicklei/thingabee/v3
 
 go 1.13
-
-require github.com/emicklei/thingabee v2.0.0+incompatible
--- ping.go
+++ ping.go
@@ -1,6 +1,6 @@
 package thingabee
 
-import "github.com/emicklei/thingabee/log"
+import "github.com/emicklei/thingabee/log/v3"
 
 func Ping() {
 	log.Say("ping")
